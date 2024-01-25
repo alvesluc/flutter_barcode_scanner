16ebc690bfc6d56f80271b9deebc316f750aacf1
Lucas Alves <42820357+alvesluc@users.noreply.github.com>
2024-01-25 08:47:11 -0300

    Update `MainActivity.java`

--- example/android/app/src/main/java/com/amolg/flutterbarcodescannerexample/MainActivity.java
+++ example/android/app/src/main/java/com/amolg/flutterbarcodescannerexample/MainActivity.java
@@ -1,8 +1,5 @@
 package com.amolg.flutterbarcodescannerexample;
 
-import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterFragmentActivity;
 
-public class MainActivity extends FlutterActivity {
-  // You can keep this empty class or remove it. Plugins on the new embedding
-  // now automatically registers plugins.
-}
+public class MainActivity extends FlutterFragmentActivity {}
